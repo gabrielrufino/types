3f891a6d514098575a17d7783ca8fb66d2acd532
Gabriel Rufino <contato@gabrielrufino.com>
2025-08-30 21:54:20 -0300

    feat: add Email and Semver types for string validation
    - Add Email<Domain> generic type with customizable domain constraint
    - Add GmailEmail type as Email<'gmail.com'> specialization
    - Add Semver type supporting standard semantic versioning format
    - Support both release versions (x.y.z) and pre-release versions (x.y.z-tag)
    - Export Semver types from main index (Email types not yet exported)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './types/json-value'
+export * from './types/semver'
 
 export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
---
+++ src/types/semver.ts
@@ -0,0 +1,3 @@
+export type Semver
+    = | `${number}.${number}.${number}`
+      | `${number}.${number}.${number}-${string}`
