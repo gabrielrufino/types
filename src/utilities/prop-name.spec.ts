edfd2e66d5277323992d4bbba635cb6f0b39cde1
Gabriel Rufino <contato@gabrielrufino.com>
2026-01-19 20:59:38 -0300

    feat: prop name

---
+++ src/utilities/prop-name.spec.ts
@@ -0,0 +1,20 @@
+import type { PropName } from './prop-name'
+
+interface Example {
+  id: number
+  name: string
+  isActive: boolean
+}
+
+type Result = PropName<Example>
+
+// The expected type is 'id' | 'name' | 'isActive'
+const testId: Result = 'id'
+const testName: Result = 'name'
+const testIsActive: Result = 'isActive'
+
+// eslint-disable-next-line no-console
+console.log(testId, testName, testIsActive)
+
+// The following lines should cause TypeScript errors if uncommented
+// const testInvalid: Result = 'invalid' // Error: Type '"invalid"' is not assignable to type 'Result'.
